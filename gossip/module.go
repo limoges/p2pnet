56bb6c23d9068a4bd30128a8ee30d2fce64b37af
Julien Limoges <julien@limoges.co>
2016-08-04 16:32:45 +0200

    Improved Gossip module Added message implementation for Gossip messages Refactored gossip module's implementation

---
+++ gossip/module.go
@@ -0,0 +1,65 @@
+package gossip
+
+import (
+	"net"
+
+	"github.com/limoges/p2pnet/cfg"
+	"github.com/limoges/p2pnet/msg"
+)
+
+type Gossip struct {
+	CacheSize     int
+	MaxConns      int
+	BootstrapAddr string
+	ListenAddr    string
+	APIAddr       string
+}
+
+const (
+	PeerInformation   = 100
+	cacheSizeToken    = "cache_size"
+	DefaultCacheSize  = 50
+	maxConnsToken     = "max_connections"
+	DefaultMaxConns   = 3
+	bootstrapToken    = "bootstrapper"
+	DefaultBootstrap  = "fulcrum.net.in.tum.de:6001"
+	listenAddrToken   = "listen_address"
+	DefaultListenAddr = "127.0.0.1:7001"
+	apiAddrToken      = "api_address"
+	DefaultApiAddr    = "127.0.0.1:6001"
+	ModuleToken       = "GOSSIP"
+)
+
+func New(conf *cfg.Configurations) (module *Gossip, err error) {
+
+	module = &Gossip{}
+	conf.Init(&module.CacheSize, ModuleToken, cacheSizeToken, DefaultCacheSize)
+	conf.Init(&module.MaxConns, ModuleToken, maxConnsToken, DefaultMaxConns)
+	conf.Init(&module.BootstrapAddr, ModuleToken, bootstrapToken, DefaultBootstrap)
+	conf.Init(&module.ListenAddr, ModuleToken, listenAddrToken, DefaultListenAddr)
+	conf.Init(&module.APIAddr, ModuleToken, apiAddrToken, DefaultApiAddr)
+	return module, nil
+}
+
+func (m *Gossip) Name() string {
+	return ModuleToken
+}
+
+func (m *Gossip) Addresses() (APIAddr, P2PAddr string) {
+	return m.APIAddr, m.ListenAddr
+}
+
+func (m *Gossip) Run() error {
+
+	select {
+	// Let's the process idle.
+	}
+}
+
+func (m *Gossip) Handle(source net.Conn, message msg.Message) error {
+
+	switch message.(type) {
+	default:
+		return p2pnet.ErrModuleDosNotHandle
+	}
+}
--- msg/gossip.go
+++ msg/gossip.go
@@ -1,5 +1,11 @@
 package msg
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+)
+
 const (
 	GOSSIP_ANNOUNCE     = 500
 	GOSSIP_NOTIFY       = 501
@@ -8,29 +14,143 @@ const (
 	// Reserved up to 519.
 )
 
-// Gossip Announce
 type GossipAnnounce struct {
 	TTL      uint8
-	reserved uint8
+	Reserved uint8
 	DataType uint16
 	Data     []byte
 }
 
-// Gossip Notify
+func (m GossipAnnounce) TypeId() uint16 {
+	return GOSSIP_ANNOUNCE
+}
+
+func NewGossipAnnounce(data []byte) (GossipAnnounce, error) {
+
+	var m GossipAnnounce
+	var reader *bytes.Reader
+	var err error
+
+	m = GossipAnnounce{}
+	reader = bytes.NewReader(data)
+
+	if err = binary.Read(reader, binary.BigEndian, &m.TTL); err != nil {
+		return m, err
+	}
+	if err = binary.Read(reader, binary.BigEndian, &m.Reserved); err != nil {
+		return m, err
+	}
+	if err = binary.Read(reader, binary.BigEndian, &m.DataType); err != nil {
+		return m, err
+	}
+
+	m.Data = make([]byte, reader.Len())
+	if _, err = io.ReadFull(reader, m.Data); err != nil {
+		return m, err
+	}
+
+	return m, nil
+}
+
 type GossipNotify struct {
-	reserved uint16
+	Reserved uint16
 	DataType uint16
 }
 
-// Gossip Notification
+func (m GossipNotify) TypeId() uint16 {
+	return GOSSIP_NOTIFY
+}
+
+func NewGossipNotify(data []byte) (GossipNotify, error) {
+
+	var m GossipNotify
+	var reader *bytes.Reader
+	var err error
+
+	m = GossipNotify{}
+	reader = bytes.NewReader(data)
+
+	if err = binary.Read(reader, binary.BigEndian, &m.Reserved); err != nil {
+		return m, err
+	}
+	if err = binary.Read(reader, binary.BigEndian, &m.DataType); err != nil {
+		return m, err
+	}
+
+	return m, nil
+}
+
 type GossipNotification struct {
-	HeaderID uint16
+	HeaderId uint16
 	DataType uint16
 	Data     []byte
+}
+
+func (m GossipNotification) TypeId() uint16 {
+	return GOSSIP_NOTIFICATION
+}
+
+func NewGossipNotification(data []byte) (GossipNotification, error) {
+
+	var m GossipNotification
+	var reader *bytes.Reader
+	var err error
+
+	m = GossipNotification{}
+	reader = bytes.NewReader(data)
+
+	if err = binary.Read(reader, binary.BigEndian, &m.HeaderId); err != nil {
+		return m, err
+	}
+	if err = binary.Read(reader, binary.BigEndian, &m.DataType); err != nil {
+		return m, err
+	}
+
+	m.Data = make([]byte, reader.Len())
+	if _, err = io.ReadFull(reader, m.Data); err != nil {
+		return m, err
+	}
+
+	return m, nil
 }
 
 type GossipValidation struct {
-	MessageID uint16
-	reserved  uint16
-	Valid     bool
+	MessageId uint16
+	Reserved  uint16
+}
+
+func (m GossipValidation) Valid() bool {
+	return true
+}
+
+func (m *GossipValidation) SetValid(valid bool) {
+	if valid {
+		m.Reserved = 0x1
+	}
+	else {
+		m.Reserved = 0x0
+	}
+}
+
+func (m GossipValidation) TypeId() uint16 {
+	return GOSSIP_VALIDATION
+}
+
+func NewGossipValidation(data []byte) (GossipValidation, error) {
+
+	var m GossipValidation
+	var reader *bytes.Reader
+	var err error
+
+	m = GossipNotification{}
+	reader = bytes.NewReader(data)
+
+	if err = binary.Read(reader, binary.BigEndian, &m.MessageId); err != nil {
+		return m, err
+	}
+	if err = binary.Read(reader, binary.BigEndian, &m.Reserved); err != nil {
+		return m, err
+	}
+
+	return m, nil
 }
